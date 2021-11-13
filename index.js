5649f1aaae0e672e4fc307bf1f4416e2bea2ebc6
Caree Youngman <caree@caree.codes>
2021-11-13 12:46:22 -0600

    formatting fix

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const tempSpan = document.querySelector("#temp");
 
 fetch(
      "http://api.openweathermap.org/data/2.5/weather?q=wylie&appid=9deb864f7791561844d409a3e4e3be56&units=imperial"
-).then(response => response.json())
+)
+.then(response => response.json())
 .then(body => {
      console.log(body);
      citySpan.innerText = body.name;
